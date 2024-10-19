b9a2c8abf94c60451fb43cbdc3a3a322997f2cc8
Avdulova <ttasina00@mail.ru>
2024-10-19 13:10:57 +0300

    ТЗ 11 спринта

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class FilmControllerTest {
+    @Autowired
     FilmController filmController;
 
     @AfterEach
